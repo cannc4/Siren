701ed09cab5a25e681110acc4b7ed54fc2098e8a
Can Inceagit config --global user.email cannc4@gmail.com <cannc4@gmail.com>
2017-04-18 08:56:44 +0100

    package update

--- config/tidal-boot-default.hs
+++ config/tidal-boot-default.hs
@@ -20,6 +20,7 @@ d_OSC <- openUDP "127.0.0.1" 12000
 devices <- midiDevices
 
 m1 <- midiStream devices "USB MIDI Device Port 1" 1 synthController
+
 (c1,ct1) <- dirtSetters getNow
 (c2,ct2) <- dirtSetters getNow
 (c3,ct3) <- dirtSetters getNow
