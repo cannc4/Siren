e03170d155edb73e69cca920b3636702bdeb8005
cannc4 <can@ince.io>
2017-05-30 22:52:49 +0100

    updatePatterns

--- src/reducers/global.js
+++ src/reducers/global.js
@@ -1,5 +1,5 @@
 const modelName = 'GLOBAL';
-const INITIAL_STATE = { globalTransformations: '', globalCommands: ''};
+const INITIAL_STATE = { globalTransformations: '', globalCommands: '', storedGlobals: []};
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'UPDATE_'+modelName:
@@ -7,11 +7,6 @@ export default (state = INITIAL_STATE, action) => {
       p.globalCommands = action.command;
       p.globalTransformations = action.tranform;
       return {...p}
-    // case 'PATTERN_'+modelName:
-    //   const z = state;
-    //   z.patList = action.sp;
-    //   console.log(action.sp);
-    //   return {...z}
     default:
       return state;
   }
